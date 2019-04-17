0af8451c31dd44fd75c229eaa8886e9e0955aef8
José Soares <josephjsoares@aecastrodaire.com>
2019-04-16 18:07:30 -0700

    Installation of waf00f and padding

--- install.sh
+++ install.sh
@@ -1,21 +1,33 @@
+echo ""
+echo ""
 echo "Welcome to the installer"
 
 cd ~
 
+echo ""
 echo "Makind Sectools dir"
 mkdir ~/Desktop/Sectools
 
-
+echo ""
 echo "Installing nmap"
 sudo apt-get install nmap
 
+echo ""
 echo "Installing curl"
 sudo apt-get install curl
 
+echo ""
 echo "Installing Security headers checker"
 wget https://raw.githubusercontent.com/meliot/shcheck/master/shcheck.py
 mv shcheck.py ~/Desktop/Sectools/shcheck.py
 
-#TODO: install waf00f
+echo ""
+echo "Installing wafw00f"
+pip install wafw00f
+
+echo ""
+echo ""
+echo ""
+echo "All done here"
 
 #dir scanner will be installed later
